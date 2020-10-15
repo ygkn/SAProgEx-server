adf474c743411ebaff7b73f36a2008afeee34cd0
ygkn <2000ygkn0713@gmail.com>
2020-10-16 01:16:08 +0900

    演習課題4

---
+++ cgi-bin/formtest.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+from cgi import FieldStorage
+from html import escape
+
+
+def main_content() -> str:
+    form_data = FieldStorage()
+    param_str: str = form_data.getvalue("param1", "")
+
+    if param_str == "":
+        return """
+            <form name="form1" action="#" method="post">
+                文字を入力して下さい
+                <input type="text" name="param1" />
+                <button type="submit" name="submit">送信</button>
+            </form>
+            """
+    else:
+        return f"""
+            <p>入力された文字は<b>「{escape(param_str)}」</b>です</p>
+            """
+
+
+# HTTP Header
+print("Content-type: text/html\n")
+
+
+# Body
+print(
+    f"""
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ふぉーむてすと</title>
+  </head>
+  <body>
+    {main_content()}
+  </body>
+</html>
+
+"""
+)
